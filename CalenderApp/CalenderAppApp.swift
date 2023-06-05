218d731b0ac61e0c096aeccab8a4f906895db00e
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-06-05 09:39:07 +0900

    Initial Commit

---
+++ CalenderApp/CalenderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalenderAppApp.swift
+//  CalenderApp
+//
+//  Created by 渡邊魁優 on 2023/06/05.
+//
+
+import SwiftUI
+
+@main
+struct CalenderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
